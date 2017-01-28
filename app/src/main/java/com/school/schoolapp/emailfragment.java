27682d9642aa0e1c21c13b6860b5ea63be2bf0dc
sebahaddin <sebahaddin15@live.nl>
2017-01-28 18:47:58 +0100

    verschillende functies openen op een pagina

---
+++ app/src/main/java/com/school/schoolapp/emailfragment.java
@@ -0,0 +1,29 @@
+package com.school.schoolapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class emailfragment extends Fragment {
+
+
+    public emailfragment() {
+        // Required empty public constructor
+    }
+//hey
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_emailfragment, container, false);
+    }
+
+}
